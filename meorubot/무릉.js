452b55e274a720db2163d129c4d70cbf8fb1b900
Sir Kid Dragon <97082454+AKidDragon@users.noreply.github.com>
2022-01-20 15:09:13 +0900

    무릉.js 파일 업데이트
    무릉 신규버전으로 업데이트됨

--- meorubot/무릉.js
+++ meorubot/무릉.js
@@ -1,10 +1,13 @@
-const scriptName="무릉.js";
+const scriptName="무릉.js"; //https://www.inven.co.kr/board/maple/2304/17003 출처 댓글쪽으로 확인
 const room_name=["메이플 머루길드 개발진"];  //여기에 채팅방 이름을 입력하세요
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
-   if(room_name.indexOf(room)!=-1 && msg.indexOf('!무릉 ') != -1){
+
+   if(room !=("방이름") && msg.indexOf('!무릉 ') != -1){
       var murung=msg.split(' ');
-      var url = Utils.getWebText("https://maple.gg/u/"+murung[1]);
+      var url = Utils.getWebText("https://maple.gg/u/%22+murung[1]   );
+
+
       if(url.indexOf('검색결과가 없습니다.') != -1){
          replier.reply('[' + murung[1] + ']\n' + '존재하지 않는 캐릭터 입니다.');
          return;
@@ -20,12 +23,18 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
       }
 
       else{
-         var info = data.split('초')[1].split('월드')[0];
+         var info = data.split('층')[2].split('월드')[0];
          var floor = data.split('록')[1].split('층')[0];
          var time = data.split('층')[1].split('Lv')[0];
-         var date = data.split('기준일:')[1];
+         var date = data.split('위')[2];
+         date = date.replace('기준일:', ' ')
+         var old = data.split('예전 무릉 최고')[0].split('Lv')[1]
+         old = old.replace('.', 'Lv.')
+         old = old.replace('/', '\n직업 :')
+         old = old.replace('기준일:', '\n기준일 :')
+         old = old.replace('">예전무릉최고', '\n최고 기록 :')
 
-         replier.reply('[' + murung[1] + ']\n' + info + '\n기록: ' + floor + '층\n시간: ' + time + '\n날짜: ' + date);
+         replier.reply('[' + murung[1] + ']\n' + info + '\n기록: ' + floor + '층\n시간: ' + time + '\n날짜: ' + date + '\n\n<예전 무릉> \n레벨 : ' + old);
       }
    }
 }
