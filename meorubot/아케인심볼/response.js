644bd7f9c7d79671b7e0aba35184770d22e666ed
Kid Dragon <kddr3039@gmail.com>
2022-02-08 17:09:38 +0900

    아케인심볼, 어센틱심볼 시동어 추가 및 !온도 명령어 추가

--- meorubot/아케인심볼/response.js
+++ meorubot/아케인심볼/response.js
@@ -1,8 +1,7 @@
 const room_name=["메이플 머루길드 개발진"];  //여기에 채팅방 이름을 입력하세요
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
-   if(room_name.indexOf(room)!=-1 && msg.indexOf('!심볼 ') != -1){
-      msg=msg.replace('!심볼 ','');
+   if(room_name.indexOf(room)!=-1 && (msg.indexOf('!아케인심볼 ') != -1 || msg.indexOf('!아케인 ') != -1 || msg.indexOf('!심볼 ') != -1)){
       var symbol=msg.split(' ');
       var total_req=0;
       var total_meso=0;
@@ -10,19 +9,19 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
       var total_meso3=0;
       var total_meso4=0;
 
-      if(symbol.length != 2){
+      if(symbol.length != 3){
          replier.reply('두개의 수를 입력해야 합니다.');
          return;
       }
 
-      for(i=0;i<2;i++){
+      for(i=1;i<3;i++){
          if(symbol[i]<1 || symbol[i]>20){
             replier.reply( '1~20의 수를 입력해야 합니다.');
             return;
          }
       }
 
-      for (i=Number(symbol[0]);i<Number(symbol[1]);i++){
+      for (i=Number(symbol[1]);i<Number(symbol[2]);i++){
          total_req+=i*i+11;
          total_meso+=3110000+3960000*i;
          total_meso2+=6220000+4620000*i;
@@ -35,7 +34,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
       total_meso3=total_meso3.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       total_meso4=total_meso4.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-      replier.reply(symbol[0]+'에서 '+symbol[1]+'레벨까지\n요구량:'+total_req+'\n여로:'+total_meso+'메소\n츄츄 :'+total_meso2+'메소\n레헬른 :'+total_meso3+'메소\n그외 나머지 :'+total_meso4+'메소');
+      replier.reply(symbol[1]+'에서 '+symbol[2]+'레벨까지\n요구량:'+total_req+'\n여로:'+total_meso+'메소\n츄츄 :'+total_meso2+'메소\n레헬른 :'+total_meso3+'메소\n그외 나머지 :'+total_meso4+'메소');
    }
 }
 /*심볼 ~에서 ~레벨까지
--- meorubot/어센틱심볼/response.js
+++ meorubot/어센틱심볼/response.js
@@ -1,26 +1,25 @@
 const room_name=["메이플 머루길드 개발진"];  //여기에 채팅방 이름을 입력하세요
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
-   if(room_name.indexOf(room)!=-1 && msg.indexOf('!어센틱 ') != -1){
-      msg=msg.replace('!어센틱 ','');
+   if(room_name.indexOf(room)!=-1 && (msg.indexOf('!어센틱 ') != -1 || msg.indexOf('!어센틱심볼 ') != -1 || msg.indexOf('!심볼2 ') != -1)){
       var symbol=msg.split(' ');
       var total_req=0;
       var total_meso=0;
       var total_meso2=0;
 
-      if(symbol.length != 2){
+      if(symbol.length != 3){
          replier.reply('두개의 수를 입력해야 합니다.');
          return;
       }
 
-      for(i=0;i<2;i++){
+      for(i=1;i<3;i++){
          if(symbol[i]<1 || symbol[i]>11){
             replier.reply( '1~11의 수를 입력해야 합니다.');
             return;
          }
       }
 
-      for (i=Number(symbol[0]);i<Number(symbol[1]);i++){
+      for (i=Number(symbol[1]);i<Number(symbol[2]);i++){
          total_req+=i*i+11;
          total_meso+=96900000+88500000*i;
          total_meso2+=106600000+97300000*i;
@@ -29,7 +28,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
       total_meso=total_meso.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       total_meso2=total_meso2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-      replier.reply(symbol[0]+'에서 '+symbol[1]+'레벨까지\n요구량:'+total_req+'\n세르니움:'+total_meso+'메소\n아르크스:'+total_meso2+'메소');
+      replier.reply(symbol[1]+'에서 '+symbol[2]+'레벨까지\n요구량:'+total_req+'\n세르니움:'+total_meso+'메소\n아르크스:'+total_meso2+'메소');
    }
 }
 
